5c175c26d562815af31b3ad55d124e8a79b43322
Emmanuel ERNEST <ernest.emmanuel@hotmail.fr>
2016-10-27 03:21:17 +0200

    scaffold username

---
+++ imports/ui/components/UserName/UserName.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserName extends Component {
+    render() {
+        return(
+            <div>
+                <h1>UserName</h1>
+            </div>
+        );
+    }
+}
+
+export default UserName;
---
+++ imports/ui/components/UserName/UserNameContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import UserName from './UserName';
+
+class UserNameContainer extends Component {
+    render() {
+        return(
+            <div>
+                <UserName />
+            </div>
+        );
+    }
+}
+
+export default UserNameContainer;
